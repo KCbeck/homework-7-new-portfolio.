915f8de14533ee41e014065f225fbd4ed5f92783
KCbeck <50602600+KCbeck@users.noreply.github.com>
2019-11-08 15:19:43 -0600

    finalportfolio

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,13 +1,4 @@
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
+
 (function() {
 
 	var bodyEl = document.body,
--- assets/js/nivo-lightbox.js
+++ assets/js/nivo-lightbox.js
@@ -1,12 +1,3 @@
-/*
- * Nivo Lightbox v1.3.1
- * http://dev7studios.com/nivo-lightbox
- *
- * Copyright 2013, Dev7studios
- * Free to use and abuse under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- */
-
 ;(function($, window, document, undefined){
 
     var pluginName = 'nivoLightbox',
